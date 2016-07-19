fb809ef68a437e894aa8a08eefef419f1e8a393d
jltichy <jltichy@gmail.com>
2016-07-19 16:14:26 -0500

    Create basic_visualization.py

---
+++ basic_visualization.py
@@ -0,0 +1,18 @@
+rom surfer import Brain
+
+print(__doc__)
+
+"""
+Define the three important variables.
+Note that these are the first three positional arguments
+in tksurfer (and pysurfer for that matter).
+"""
+subject_id = 'fsaverage'
+hemi = 'lh'
+surface = 'inflated'
+
+"""
+Call the Brain object constructor with these
+parameters to initialize the visualization session.
+"""
+brain = Brain(subject_id, hemi, surface)
