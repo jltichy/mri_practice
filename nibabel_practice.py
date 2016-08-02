ca8847aa8f8947aacbe787e4e86d37b002651203
jltichy <jltichy@gmail.com>
2016-08-02 20:45:13 +0000

    update practice file

--- nibabel_practice.py
+++ nibabel_practice.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # This is an introduction to nibabel from the site: http://nipy.org/nibabel/gettingstarted.html
 
 import os
@@ -15,11 +17,11 @@ data = img.get_data()
 type(data)
 hdr = img.header
 raw = hdr.structarr
-array(10, dtype=uint8)
+
 
 data = np.ones((32, 32, 15, 100), dtype=np.int16)
 img = nib.Nifti1Image(data, np.eye(4))
 img.get_data_dtype() == np.dtype(np.int16)
 img.header.get_xyzt_units()
 
-img.to_filename(os.path.join('build','test4d.nii.gz'))
+nib.save(img, os.path.join('build','test4d.nii.gz'))
